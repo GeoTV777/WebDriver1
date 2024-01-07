8cdd5a0a4303851d213de5436d816cfd6f90c7e7
GeoTV777 <ivushka777@mail.ru>
2024-01-07 23:01:13 +0300

    07/01/24 3 tests code = 0

--- src/test/java/WebDriverDz.java
+++ src/test/java/WebDriverDz.java
@@ -2,6 +2,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 //import org.apache.logging.log4j.LogManager;
 //import org.apache.logging.log4j.core.Logger;
 //import org.checkerframework.checker.units.qual.A;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,7 +15,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.time.Duration;
 
 public class WebDriverDz {
-//    static Logger logger = LogManager.getLogger(FirstTest.class);
+//    static Logger logger = (Logger) LogManager.getLogger(FirstTest.class);
     WebDriver driver;
 
     @BeforeAll
@@ -22,23 +25,39 @@ public class WebDriverDz {
     }
 
     @BeforeEach
-    public void driverInstall() {
+    public void driverInstallFoHeadless() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-//       открыть Chrome в headless режиме
+//       открыть Chrome в headless режиме headless
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
 
+    }
 
-//      driver = new ChromeDriver();
-//        logger.info("Запуск драйвера");
+    @BeforeEach
+    public void driverInstallFoKiosk(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        //открыть Chrome в режиме киоска
+        options.addArguments("--enable-hardwere-averlays");
+        driver = new ChromeDriver(options);
+    }
 
+    @BeforeEach
+    public void driverFullScreen() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments();
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+//        logger.info(" ");
     }
 
     @AfterEach
     public void driverStop() {
         if (driver != null) {
-//            logger.info("Закрытие браузера");
+//           logger.info("Закрытие браузера");
             driver.close();
             driver.quit();
         }
@@ -55,12 +74,61 @@ public class WebDriverDz {
         WebElement elInput = driver.findElement(By.cssSelector("#searchbox_input"));
         elInput.sendKeys("ОТУС");
         elEnter.submit();
-//        В открывшемся окне проверяем, что в поисковой выдаче первый результат Онлайн‑курсы для профессионалов, дистанционное обучение
+//        В открывшемся окне проверяем, что в поисковой выдаче первый результат Онлайн‑курсы для профессионалов,
+//        дистанционное обучение
+//        Если не добавить окончание фразы - "современным ...", то не сработает
+//        По заданию точно ли выдача поисковой строки ДОЛЖНА совпадать с ожидаемым результатом?))))))
 
         driver.get("https://duckduckgo.com/?t=h_&q=%D0%BE%D1%82%D1%83%D1%81&ia=web/");
         WebElement elResult = driver.findElement(By.xpath("//*[@id='r1-0']/div[2]/h2/a/span"));
-        Assertions.assertEquals("Онлайн‑курсы для профессионалов, дистанционное обучение современным ...", elResult.getText());
-
+        Assertions.assertEquals("Онлайн‑курсы для профессионалов, дистанционное обучение современным ...",
+                elResult.getText());
+    }
+    @Test
+    public void openFullscreen() {
+//       Переходим на сайт:
+        driver.get("https://p.w3layouts.com/demos_new/template_demo/03-10-2020/photoflash-liberty-demo_Free/685659620/web/index.html?_ga=2.181802926.889871791.1632394818-2083132868.1632394818");
+//       Кликаем картинку
+//        WebElement elPicture = driver.findElement(By.xpath("/html/body/section[2]/div/ul[2]/li[7]/span/a/div[2]/h3"));
+//        elPicture.click();
+////        Проверяем открытие картинки в модальном окне
+//        WebElement elModalPicture = driver.findElement(By.cssSelector(".pp_hoverContainer"));
+//        Assertions.assertTrue(elModalPicture.isDisplayed(),"Нет изображения в модальном окне");
+    }
+    @Test
+    public void openMaximaze() {
+//        Открыть Chrome в режиме полного экрана
+//        Перейти на https://otus.ru
+        driver.get("https://otus.ru");
+//        Авторизоваться под каким-нибудь тестовым пользователем(можно создать нового)
+//      log:tvgeo777@gmail.com
+//      pass: tvGEO777#777
+        autorization();
+//        Вывести в лог все cookie
+//
     }
 
+    private void autorization() {
+        WebElement elEnter = driver.findElement(By.xpath("//*[@id=\"__next\"]//section/div[1]/button"));
+        elEnter.click();
+
+        WebElement elFocus = driver.findElement(By.xpath("//input[@name]/.."));
+        elFocus.click();
+        WebElement elEmail = driver.findElement(By.xpath("//input[@name]"));
+        elEmail.sendKeys("tvgeo777@gmail.com");
+
+        elFocus = driver.findElement(By.xpath("//input[@type=\"password\"]/.."));
+        elFocus.click();
+        WebElement elPassword = driver.findElement(By.xpath("//input[@type=\"password\"]"));
+        elPassword.sendKeys("tvGEO777#777");
+        elEnter = driver.findElement(By.xpath("//button[./*[text() = 'Войти']]"));
+        elEnter.click();
+        try {
+            Thread.sleep(5000);
+        }
+        catch (Exception ignored){}
+    }
 }
+
+
+
