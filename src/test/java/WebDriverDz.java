19897a220ff68c223694b71e7b9a705dd0d715de
GeoTV777 <ivushka777@mail.ru>
2024-01-08 00:02:49 +0300

    07/01/24 3 tests ((

--- src/test/java/WebDriverDz.java
+++ src/test/java/WebDriverDz.java
@@ -89,10 +89,10 @@ public class WebDriverDz {
 //       Переходим на сайт:
         driver.get("https://p.w3layouts.com/demos_new/template_demo/03-10-2020/photoflash-liberty-demo_Free/685659620/web/index.html?_ga=2.181802926.889871791.1632394818-2083132868.1632394818");
 //       Кликаем картинку
-//        WebElement elPicture = driver.findElement(By.xpath("/html/body/section[2]/div/ul[2]/li[7]/span/a/div[2]/h3"));
+//        WebElement elPicture = driver.findElement(By.xpath("/html/body/section[2]/div/ul[2]/li[7]/span"));
 //        elPicture.click();
-////        Проверяем открытие картинки в модальном окне
-//        WebElement elModalPicture = driver.findElement(By.cssSelector(".pp_hoverContainer"));
+//////        Проверяем открытие картинки в модальном окне
+//        WebElement elModalPicture = driver.findElement(By.xpath("/html/body/div[7]/div[3]/div/div/div/div[2]/div[3]"));
 //        Assertions.assertTrue(elModalPicture.isDisplayed(),"Нет изображения в модальном окне");
     }
     @Test
