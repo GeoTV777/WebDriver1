b64f1b3197e5a748c22cb082f9205130f2ff9967
GeoTV777 <ivushka777@mail.ru>
2024-01-20 17:15:05 +0300

    20/01/24

--- src/test/java/FullscreenTest.java
+++ src/test/java/FullscreenTest.java
@@ -1,30 +1,51 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.WebElement;
 
-public class FullscreenTest extends WebDriverDz{
+public class FullscreenTest {
+    static Logger logger = (Logger) LogManager.getLogger("WebDriverDZ");
+
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void driverSetup() {
+        WebDriverManager.chromedriver().setup();
+        logger.info("Установка драйвера");
+    }
+
     @BeforeEach
     public void driverInstallFoKiosk(){
         ChromeOptions options = new ChromeOptions();
 //      Открыть Chrome в режиме киоска
         options.addArguments("--start-fullscreen");
         driver = new ChromeDriver(options);
-        logger.info("Открытие браузера в режиме киоска");
+        logger.info("Открытие браузера в режиме fullscreen");
+    }
+
+    @AfterEach
+    public void driverStop() {
+        if (driver != null) {
+            logger.info("Закрытие браузера");
+            driver.close();
+            driver.quit();
+        }
+
     }
     @Test
     public void openFullscreen() {
 //      Переходим на сайт:
         driver.get("https://p.w3layouts.com/demos_new/template_demo/03-10-2020/photoflash-liberty-demo_Free/685659620/web/index.html?_ga=2.181802926.889871791.1632394818-2083132868.1632394818");
 //      Кликаем картинку
-        WebElement elPicture = driver.findElement(By.xpath("//h3[text()='This is a title']"));
+        WebElement elPicture = driver.findElement(By.xpath("//li[contains(@data-id,'id-1')]//a"));
         elPicture.click();
 //      Проверяем открытие картинки в модальном окне
-        WebElement elModalPicture = driver.findElement(By.xpath("//img[contains(@src,'p7.jpg')]"));
-        Assertions.assertTrue(elModalPicture.isDisplayed(),"Нет изображения в модальном окне");
+        WebElement elModalPicture = driver.findElement(By.cssSelector(".pp_content"));
+        Assertions.assertTrue(elModalPicture.isDisplayed(),"No image in modal window");
     }
 }
-
--- src/test/java/MaximizeTest.java
+++ src/test/java/MaximizeTest.java
@@ -1,13 +1,28 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import java.time.Duration;
 
+public class MaximizeTest {
+    static Logger logger = (Logger) LogManager.getLogger("WebDriverDZ");
 
-public class MaximizeTest extends WebDriverDz {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void driverSetup() {
+        WebDriverManager.chromedriver().setup();
+        logger.info("Установка драйвера");
+    }
     @BeforeEach
     public void driverMiximize() {
         ChromeOptions options = new ChromeOptions();
@@ -18,39 +33,45 @@ public class MaximizeTest extends WebDriverDz {
         logger.info("Открытие браузера в режиме полного окна");
     }
 
-   @Test
-   public void openMaximaze() {
+    @AfterEach
+    public void driverStop() {
+        if (driver != null) {
+            logger.info("Закрытие браузера");
+            driver.close();
+            driver.quit();
+        }
+    }
+    @Test
+    public void openMaximaze() {
 //     Открыть Chrome в режиме полного экрана
 //     Перейти на https://otus.ru
-       driver.get("https://otus.ru");
+        driver.get("https://otus.ru");
 //     Авторизоваться под каким-нибудь тестовым пользователем(можно создать нового)
 //     log:tvgeo777@gmail.com
 //     pass: tvGEO777#777
-       autorization();
+        autorization();
 //     Вывести в лог все cookie
-       System.out.println("Это куки: " + driver.manage().getCookies());
+        System.out.println("Это куки: " + driver.manage().getCookies());
 //
-   }
-
-   private void autorization() {
+    }
+    private void autorization() {
 //     Нажимаем кнопку Войти на главной странице
-       WebElement elEnter = driver.findElement(By.xpath("//button[text()='Войти']"));
-       elEnter.click();
+        WebElement elEnter = driver.findElement(By.xpath("//button[text()='Войти']"));
+        elEnter.click();
 //     На странице регистрации находим и кликаем поле Электронная почта
-       WebElement elFocus = driver.findElement(By.xpath("//input[@name]/.."));
-       elFocus.click();
+        WebElement elFocus = driver.findElement(By.xpath("//input[@name]/.."));
+        elFocus.click();
 //     Вводим email в поле Электронная почта
-       WebElement elEmail = driver.findElement(By.xpath("//input[@name]"));
-       elEmail.sendKeys("tvgeo777@gmail.com");
+        WebElement elEmail = driver.findElement(By.xpath("//input[@name]"));
+        elEmail.sendKeys("tvgeo777@gmail.com");
 //     На странице регистрации находим и кликаем поле Пароль
-       elFocus = driver.findElement(By.xpath("//input[@type='password']/.."));
-       elFocus.click();
+        elFocus = driver.findElement(By.xpath("//input[@type='password']/.."));
+        elFocus.click();
 //     Вводим пароль в поле Пароль
-       WebElement elPassword = driver.findElement(By.xpath("//input[@type='password']"));
-       elPassword.sendKeys("tvGEO777#777");
+        WebElement elPassword = driver.findElement(By.xpath("//input[@type='password']"));
+        elPassword.sendKeys("tvGEO777#777");
 //     Нажимаем кнопку Войти
-       elEnter = driver.findElement(By.xpath("//button[./*[text()='Войти']]"));
-       elEnter.click();
-   }
+        elEnter = driver.findElement(By.xpath("//button[./*[text()='Войти']]"));
+        elEnter.click();
+    }
 }
-
